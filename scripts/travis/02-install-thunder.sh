fbb990454cc390cfde6816d3d1a7863df3edcaf1
Coheed <rachidselma@googlemail.com>
2020-08-11 11:46:48 +0200

    Init #0820

--- scripts/travis/02-install-thunder.sh
+++ scripts/travis/02-install-thunder.sh
@@ -2,5 +2,4 @@
 
 composer install
 
-cd docroot
-/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder --db-url=mysql://travis@127.0.0.1/thunder -y
+/usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" drush si thunder --root=docroot --db-url=mysql://travis@127.0.0.1/thunder -y
